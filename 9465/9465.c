7a59433a033211fa530666d51bbd45b881d60d28
Jeonghyuk Park <chris.jeonghyuk.park@gmail.com>
2019-06-18 11:01:04 +0100

    complete 9465

--- 9465/9465.c
+++ 9465/9465.c
@@ -4,11 +4,13 @@
  *
  * 
  * */
+int max(int a, int b);
+
 int main(void)
 {
   int t, n, i, j;
   int arr[2][100001] = {0,};
-  int max[2][100001] = {0,};
+  int total[2][100001] = {0,};
 
   scanf("%d", &t);
 
@@ -17,28 +19,33 @@ int main(void)
     // number of row
     scanf("%d", &n);
     // get array
-    for(j=0; j<n; j++)
+    for(j=1; j<=n; j++)
     {
       scanf("%d", &arr[0][j]);
     }
-    for(j=0; j<n; j++)
+    for(j=1; j<=n; j++)
     {
       scanf("%d", &arr[1][j]);
     }
-
     //initial
-    max[0][0] = 0;
-    max[1][0] = 0;
-    max[0][1] = arr[0][1];
-    max[1][1] = arr[1][1];
+    total[0][0] = 0;
+    total[1][0] = 0;
+    total[0][1] = arr[0][1];
+    total[1][1] = arr[1][1];
     //recurrence
-    for(j=2; j<n; j++)
+    for(j=2; j<=n; j++)
     {
-      max[0][j] = arr[0][j] + max(max[1][j-1], max[1][j-2]);
-      max[1][j] = arr[1][j] + max(max[0][j-1], max[0][j-2]);
+      total[0][j] = arr[0][j] + max(total[1][j-1], total[1][j-2]);
+      total[1][j] = arr[1][j] + max(total[0][j-1], total[0][j-2]);
     }
+    printf("%d\n", max(total[0][n], total[1][n]));
   }
+
 
   return 0;
 }
  
+int max(int a, int b)
+{
+  return a > b ? a : b;
+}
